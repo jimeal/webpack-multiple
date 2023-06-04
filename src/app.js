af6d832ad61b54107efbd229fee54904480b31e8
Bae Ha-eun <dncl46@gmail.com>
2023-06-04 22:18:14 +0900

    자주사용하는 플러그인 설치 및 구성

--- src/app.js
+++ src/app.js
@@ -1,1 +1,6 @@
 import "./app.css";
+
+console.log(process.env.NODE_ENV);
+console.log(TWO);
+console.log(THREE);
+console.log(api.domain);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const path = require("path")
+const webpack = require("webpack")
+const childProcess = require("child_process");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
 module.exports = {
   mode: "development",
   entry: {
-    main: "./src/app.js",
+    main: "./src/app.js"
   },
   output: {
     filename: "[name].js",
@@ -14,29 +19,51 @@ module.exports = {
       {
         test: /\.css$/,
         use: [
-          "style-loader",
+          process.env.NODE_ENV === "production"
+            ? MiniCssExtractPlugin.loader
+            : "style-loader",
           "css-loader"
         ]
       },
-      // {
-      //   test: /\.png$/,
-      //   loader: "file-loader",
-      //   options: {
-      //     publicPath: './dist',
-      //     name: "[name].[ext]?[hash]"
-      //   }
-      // },
       {
         test: /\.(png|jpg|gif|svg)$/,
         use: {
           loader: 'url-loader',
           options: {
-            publicPath: './dist',
             name: '[name].[ext]?[hash]',
             limit: 20000
           }
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: `
+        Build Date: ${new Date().toLocaleString()}
+        Commit Version: ${childProcess.execSync('git rev-parse --short HEAD')}
+        Author: ${childProcess.execSync('git config user.name')}
+      `
+    }),
+    new webpack.DefinePlugin({
+      TWO: 1+1,
+      THREE: '1+2',
+      'api.domain': JSON.stringify('http://dev.api.domain.com')
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      templateParameters: {
+        env: process.env.NODE_ENV ==='development' ? '(dev)' : '',
+      },
+      minify: process.env.NODE_ENV === 'production' ? {
+        collapseWhitespace: true, // 빈칸 제거
+        removeComments: true, // 주석 제거
+      } : false,
+      hash: true
+    }),
+    new CleanWebpackPlugin(),
+    ...(process.env.NODE_ENV === "production"
+      ? [new MiniCssExtractPlugin({filename: `[name].css`})]
+      : []),
+  ]
 }
